2d6d3c982ed7a200d0b8331576a495e6733b6acb
Simon Stürz <simon.stuerz@nymea.io>
2025-12-17 09:44:06 +0100

    Align license headers

--- plugin/chargingsessionsdbusinterfaceclient.h
+++ plugin/chargingsessionsdbusinterfaceclient.h
@@ -5,20 +5,20 @@
 * Copyright (C) 2013 - 2024, nymea GmbH
 * Copyright (C) 2024 - 2025, chargebyte austria GmbH
 *
-* This file is part of nymea-energy-plugin-chargingsessions.
+* This file is part of nymea-experience-plugin-evdash.
 *
-* nymea-energy-plugin-chargingsessions is free software: you can redistribute it and/or modify
+* nymea-experience-plugin-evdash is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* nymea-energy-plugin-chargingsessions is distributed in the hope that it will be useful,
+* nymea-experience-plugin-evdash is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with nymea-energy-plugin-chargingsessions. If not, see <https://www.gnu.org/licenses/>.
+* along with nymea-experience-plugin-evdash. If not, see <https://www.gnu.org/licenses/>.
 *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
--- plugin/evdashengine.cpp
+++ plugin/evdashengine.cpp
@@ -1,30 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
-* Copyright 2013 - 2025, nymea GmbH
-* Contact: contact@nymea.io
+* Copyright (C) 2013 - 2024, nymea GmbH
+* Copyright (C) 2024 - 2025, chargebyte austria GmbH
 *
-* This file is part of nymea.
-* This project including source code and documentation is protected by
-* copyright law, and remains the property of nymea GmbH. All rights, including
-* reproduction, publication, editing and translation, are reserved. The use of
-* this project is subject to the terms of a license agreement to be concluded
-* with nymea GmbH in accordance with the terms of use of nymea GmbH, available
-* under https://nymea.io/license
+* This file is part of nymea-experience-plugin-evdash.
 *
-* GNU General Public License Usage
-* Alternatively, this project may be redistributed and/or modified under the
-* terms of the GNU General Public License as published by the Free Software
-* Foundation, GNU version 3. This project is distributed in the hope that it
-* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-* Public License for more details.
+* nymea-experience-plugin-evdash is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 *
-* You should have received a copy of the GNU General Public License along with
-* this project. If not, see <https://www.gnu.org/licenses/>.
+* nymea-experience-plugin-evdash is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
 *
-* For any further details and any questions please contact us under
-* contact@nymea.io or see our FAQ/Licensing Information on
-* https://nymea.io/license/faq
+* You should have received a copy of the GNU General Public License
+* along with nymea-experience-plugin-evdash. If not, see <https://www.gnu.org/licenses/>.
 *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -527,7 +521,7 @@ QJsonObject EvDashEngine::packCharger(Thing *charger) const
     chargerObject.insert("pluggedIn", charger->stateValue("pluggedIn").toBool());
     chargerObject.insert("chargingAllowed", charger->stateValue("power").toBool());
 
-    QString stateName = "power";
+    QString stateName = "currentPower";
     chargerObject.insert(stateName, charger->stateValue(stateName).toString());
     QString source = QString("state-%1-%2").arg(charger->id().toString(QUuid::WithBraces), stateName);
     LogFetchJob *job = m_logEngine->fetchLogEntries(
@@ -539,14 +533,14 @@ QJsonObject EvDashEngine::packCharger(Thing *charger) const
         0, 1                               // offset, limit
         );
 
-    connect(job, &LogFetchJob::finished, this, [](const LogEntries &entries) {
+    connect(job, &LogFetchJob::finished, this, [charger, stateName](const LogEntries &entries) {
         if (entries.isEmpty()) {
-            qCDebug(dcEvDashExperience()) << "##### Last state change unknwon";
+            qCDebug(dcEvDashExperience()) << "##### Last state change of" << charger->name() << stateName << "unknwon";
             return;
         }
 
         //qint64 lastChangeMs = entries.first().timestamp().toMSecsSinceEpoch();
-        qCDebug(dcEvDashExperience()) << "##### Last state change" << entries.first().timestamp().toString();
+        qCDebug(dcEvDashExperience()) << "##### Last state change" << charger->name() << stateName << entries.first().timestamp().toString() << entries.first().values().first();
     });
 
     if (charger->hasState("currentVersion"))
